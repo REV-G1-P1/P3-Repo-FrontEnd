8bf90d63a00b49f29b635db989f7cf68a6912ec5
Firas <firashijazi01@gmail.com>
2022-12-27 11:09:01 -0500

    configure persistant state

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -6,16 +6,13 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../Redux/Store";
 import { accountInformation } from "../../Types/AccountInformation";
 import { useEffect } from "react";
-import { User } from "../../Types/User";
 import { useNavigate } from "react-router-dom";
 
 export const HomePage:React.FC= ()=>{
     
     const userState = useSelector((state:RootState) => state.auth);
-    const user:User = JSON.parse(localStorage.getItem("user")||'{}');
     const navigate = useNavigate();
-    console.log("user home localstorage "+JSON.stringify(user));
-   // console.log("user home auth state "+JSON.stringify(userState));
+  
         const handleTransferButton= (e: { preventDefault: () => void; })=>{
             e.preventDefault();
             document.getElementById("transferPage")!.style.display="block";
@@ -33,9 +30,7 @@ export const HomePage:React.FC= ()=>{
         }
 
         useEffect( ()=>{
-            console.log("useEffect "+JSON.stringify(user.accountInformation));
-            if(!userState.currentUser){userState.currentUser=user};
-        //     console.log("home page use effect "+JSON.stringify(userState.currentUser))
+       
         },[userState.currentUser])
     
     return (
@@ -43,15 +38,15 @@ export const HomePage:React.FC= ()=>{
        
 
         <div className="HomeRootContainer">
-        <h1 className="HomePageHeader">Hello {!user?
+        <h1 className="HomePageHeader">Hello {
         userState.currentUser.firstName?.charAt(0).toUpperCase() + userState.currentUser.firstName?.slice(1)
-       :user.firstName?.charAt(0).toUpperCase() + user.firstName?.slice(1)
-     
-    }</h1>
+        }
+     </h1>
              <div className="HomeAccountContainers">
             <div className="LeftHomeContainer">
             {
-           userState.currentUser.accountInformation.length? userState.currentUser.accountInformation?.map((account:accountInformation) => {
+         
+           userState.currentUser.accountInformation?.map((account:accountInformation) => {
                
                 return <AccountPage key={account.accountNumber} 
                 accountName={''} accountNumber={account.accountNumber} 
@@ -59,15 +54,7 @@ export const HomePage:React.FC= ()=>{
                 accountType = {account.accountType}          />
                 
             })
-            :
-            user.accountInformation?.map((account:accountInformation) => {
-               
-                return <AccountPage key={account.accountNumber} 
-                accountName={''} accountNumber={account.accountNumber} 
-                routingNumber={account.routingNumber} balance= {account.balance}
-                accountType = {account.accountType}          />
-                
-            })
+           
        }
                 </div>
                 <div className="CenterHomeContainer"></div>
--- src/Redux/Store.ts
+++ src/Redux/Store.ts
@@ -1,16 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { AnyAction, combineReducers, configureStore, Reducer } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/es/storage";
 import  accountSlice  from "./Slices/AccountSlice";
 import personSlice  from "./Slices/UserSlice";
 
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+
+  const rootReducer: Reducer = (state: RootState, action: AnyAction) => {
+    if (action.type === 'auth/logout') {
+  
+      storage.removeItem('persist:root')
+  
+      state = {} as RootState
+    }
+    return appReducer(state, action)
+  }
+
+  const appReducer = combineReducers({ 
+     auth: personSlice,
+        account: accountSlice,
+  })
+  
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const Store = configureStore({
-    reducer: {
-       
-        auth: personSlice,
-        account: accountSlice,
-     
-    },
+    reducer: persistedReducer,
+    
     devTools:true
 });
+
+export const Persistor= persistStore(Store);
+
 export type RootState = ReturnType<typeof Store.getState>;
 export type DispatchType = typeof Store.dispatch;
