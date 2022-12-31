bd85eaf26064dd905b84afa72bf675b11d449d2f
christopherc360 <chriscrawford360@Outlook.com>
2022-12-31 09:46:25 -0600

    Manager Changes

--- src/Components/Manager/Manager.tsx
+++ src/Components/Manager/Manager.tsx
@@ -21,7 +21,7 @@ export const ManagerPage:React.FC= ()=>{
         dispatch(getPendingMortgages());
         setTimeout(() => {
             setShowMap(true);
-          }, 2000);
+          }, 5000);
     },[])
 
     return (
